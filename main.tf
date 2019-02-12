19ff6c8446cf49df6fc378e2f59b5ea9ca4b8352
TorstenRmeyer <47574855+TorstenRmeyer@users.noreply.github.com>
2019-02-12 20:35:06 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+
+terraform {
+	backend "s3" {
+		## Config not used here - parameters are provided as key-value pairs in init
+		## during terraform init
+		# bucket = "s3-terraform-state"
+		# key = "my-project"
+		# region = "eu-central-1"
+	}
+}
+
+##################################################################
+# Example: Show how to read data from AWS
+# Data sources to get VPC, subnet, security group and AMI details
+##################################################################
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "all" {
+  vpc_id = "${data.aws_vpc.default.id}"
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name = "name"
+
+    values = [
+      "amzn-ami-hvm-*-x86_64-gp2",
+    ]
+  }
+
+  filter {
+    name = "owner-alias"
+
+    values = [
+      "amazon",
+    ]
+  }
+}
+
+##################################################################
+# Example: Create a simple SNS - use Variable for name
+##################################################################
+resource "aws_sns" "example_sns" {
+	name= "${vars.snstopic.name}"
+}
+
